71d499c8db498d39a7443df52b3a82d22ab97a8a
jransome <jamesransome@msn.com>
2020-11-06 12:26:30 +0000

    Assign role to player

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 const path = require('path');
 const express = require('express');
 const socket = require('socket.io');
@@ -10,34 +12,79 @@ app.use(express.static(path.join(__dirname, './public')));
 const expressServer = app.listen(PORT, () => console.log('Server running on port', PORT));
 const socketServer = socket(expressServer);
 
-// const players = new Set();
-// let hostPlayer = null;
 
-// class Player {
-//   constructor(name, role){
-//     this.name = name
-//     this.role = role
-//     // avatar?
-//     // 
-//   }
+// eslint-disable
+
+const ROLES = [
+  'DEVELOPER',
+  'TECH_LEAD',
+  'HACKER',
+];
+
+class Player {
+  constructor(name, role) {
+    this.name = name
+    this.role = role
+    // avatar?
+    // 
+  }
 
 
-// }
+}
+
+class Service {
+  constructor(name) {
+    this.name = name;
+    this.lives = 3;
+    this.isImmunised = false;
+    this.players = [];
+  }
+
+  hack() {
+    if (this.isImmunised) {
+      console.log('attempted hack on', this.name, 'but was immunised');
+
+      this.isImmunised = false;
+
+      // notify assigned players of hack attempt
 
 
+    } else {
+      console.log(this.name, 'was hacked!!!!');
+      this.lives -= 1;
 
-// class Game {
+      // check if lives depleted and something
+    }
+  }
 
-// }
+  assignPlayer(player) {
+    this.players.push(player)
+
+  }
+}
+
+class Game {
+  constructor() {
+    this.players = [];
+  }
+}
+
+const players = new Set();
+
 
 socketServer.sockets.on('connect', (newSocket) => {
   console.log(newSocket.id, 'connected');
+  let playerInstance = null;
 
-  newSocket.on('login', (name) => {
+  newSocket.on('login', (name, assignRole) => {
     console.log('received login event from', name);
+    playerInstance = new Player(name, ROLES[2]);
+    players.add(playerInstance);
+    assignRole(playerInstance.role);
   });
 
   newSocket.on('disconnect', () => {
     console.log(newSocket.id, 'disconnected');
+    if (playerInstance) players.delete(playerInstance);
   });
 });
--- public/index.js
+++ public/index.js
@@ -6,21 +6,36 @@ const CANVAS_HEIGHT = 600;
 const CANVAS_WIDTH = 1000;
 const gameContainer = document.getElementById('game');
 
-
 const start = () => new P5((sketch) => {
+  // login elements
   const nameInput = sketch.createInput().attribute('placeholder', 'Enter you name');
   const nameSubmitBtn = sketch.createButton('submit');
+
+
+
+  
+
 
   // eslint-disable-next-line no-param-reassign
   sketch.setup = () => {
     sketch.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
     sketch.textAlign(sketch.CENTER);
     sketch.background(60);
-
+    
     nameSubmitBtn.mousePressed(() => {
       console.log('submit pressed', nameInput.value());
-      socket.emit('login', nameInput.value());
+      if (!nameInput.value()) return;
+      socket.emit('login', nameInput.value(), (assignedRole) => {
+        nameInput.hide();
+        nameSubmitBtn.hide();
+        
+        sketch.textSize(24);
+        const roleIndicator = sketch.text(`${nameInput.value()}: ${assignedRole}`, CANVAS_WIDTH - 150, 30);
+      });
+      // roleIndicator.show();
     });
+
+
   };
 
   // eslint-disable-next-line no-param-reassign
