e4c3f010bdc7163284b461729159a56a273d267b
miglekuba <migle.kubiliute@gmail.com>
2020-11-06 17:27:54 +0000

    Assign roles at game start

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const path = require("path");
 const express = require("express");
 const socket = require("socket.io");
+const { runInThisContext } = require("vm");
+const { listen } = require("socket.io");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -21,7 +23,7 @@ const ROLES = ["DEVELOPER", "TECH_LEAD", "HACKER"];
 class Player {
   constructor(name, role) {
     this.name = name;
-    this.role = role;
+    this.role = ROLES[0];
     // avatar?
     //
   }
@@ -58,13 +60,35 @@ class Service {
 class Game {
   constructor() {
     this.isStarted = false;
-    this.players = [];
+    this.players = new Set();
+  }
+  addPlayer(name) {
+    const playerInstance = new Player(
+      name,
+      ROLES[Math.floor(Math.random() * 3)]
+    );
+    this.players.add(playerInstance);
+    return playerInstance;
+  }
+  removePlayer(player) {
+    this.players.delete(player);
+    if (this.players.size === 0) this.end();
   }
 
   start() {
     this.isStarted = true;
     console.log("game started");
     socketServer.emit("gameStarted");
+    let chosenOnes = new Set();
+    while (chosenOnes.size < 3) {
+      chosenOnes.add(Math.floor(Math.random() * this.players.size));
+    }
+    chosenOnes = [...chosenOnes];
+    const playersArray = [...this.players];
+    playersArray[chosenOnes[0]].role = ROLES[1];
+    playersArray[chosenOnes[1]].role = ROLES[2];
+    playersArray[chosenOnes[2]].role = ROLES[2];
+    console.log(this.players);
   }
 
   end() {
@@ -72,18 +96,16 @@ class Game {
   }
 }
 
-const players = new Set();
 const game = new Game();
 
 socketServer.sockets.on("connect", (newSocket) => {
   console.log(newSocket.id, "connected");
   let playerInstance = null;
 
-  newSocket.on("login", (name, assignRole) => {
+  newSocket.on("login", (name) => {
     console.log("received login event from", name);
-    playerInstance = new Player(name, ROLES[Math.floor(Math.random() * 3)]);
-    players.add(playerInstance);
-    assignRole(playerInstance.role);
+    playerInstance = game.addPlayer(name);
+    // assignRole(playerInstance.role);
   });
 
   newSocket.on("gameStart", () => {
@@ -93,11 +115,6 @@ socketServer.sockets.on("connect", (newSocket) => {
 
   newSocket.on("disconnect", () => {
     console.log(newSocket.id, "disconnected");
-    if (playerInstance) {
-      players.delete(playerInstance);
-     if(players.size === 0) {
-       game.end();
-     }
-    }
+    if (playerInstance) game.removePlayer(playerInstance);
   });
 });
